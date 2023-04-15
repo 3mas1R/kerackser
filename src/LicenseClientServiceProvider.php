393484ef51177ca74d5850ac57e648b79a5c7ae5
Ricardo Diaz <43691108+kerackdiaz@users.noreply.github.com>
2023-04-15 11:11:33 -0500

    autoinstall

---
+++ src/LicenseClientServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kerackser\LicenseClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class LicenseClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publicar archivo de configuración
+        $this->publishes([
+            __DIR__ . '/../config/license_client.php' => config_path('license_client.php'),
+        ], 'config');
+
+        // Registrar middleware CheckLicense
+        $this->app['router']->aliasMiddleware('check_license', CheckLicense::class);
+    }
+}
