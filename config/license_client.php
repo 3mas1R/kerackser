7d41aa1cf65b1cf7c18f8890dec90d1329baf401
Ricardo Diaz <43691108+kerackdiaz@users.noreply.github.com>
2023-04-15 13:10:04 -0500

    update source

--- config/license_client.php
+++ config/license_client.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'license_server_url' => env('LICENSE_SERVER_URL', 'https://licenser.test/'),
+    'license_server_url' => env('LICENSE_SERVER_URL', 'https://licenser.3mas1r.com'),
 ];
